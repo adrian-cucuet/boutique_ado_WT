891c3e0885d8758b753d909da8040809d62cb854
adrian-cucuet <adrian.cucuet@hotmail.com>
2023-05-19 20:39:20 +0000

    Add checkout success logic

--- checkout/admin.py
+++ checkout/admin.py
@@ -2,16 +2,17 @@ from django.contrib import admin
 from .models import Order, OrderLineItem
 
 
-class OrderInLineItemAdminInLine(admin.TabularInline):
+class OrderLineItemAdminInline(admin.TabularInline):
     model = OrderLineItem
     readonly_fields = ('lineitem_total',)
 
 
 class OrderAdmin(admin.ModelAdmin):
-    inline = (OrderInLineItemAdminInLine,)
+    inlines = (OrderLineItemAdminInline,)
+
     readonly_fields = ('order_number', 'date',
-                        'delivery_cost', 'order_total',
-                        'grand_total',)
+                       'delivery_cost', 'order_total',
+                       'grand_total',)
 
     fields = ('order_number', 'date', 'full_name',
               'email', 'phone_number', 'country',
